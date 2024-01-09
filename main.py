83091a4c4e304c0119fae53d5a78a450dcec6ac4
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 23:23:28 +0100

    tried bless

---
+++ main.py
@@ -0,0 +1,10 @@
+from bless import BlessServer
+import asyncio
+
+from bless.backends.characteristic import BlessGATTCharacteristic
+
+server = BlessServer(name="Test-BLE-Server", loop=asyncio.get_event_loop())
+server.read_request_func = lambda characteristic, **kwargs: print(characteristic)
+server.write_request_func = lambda characteristic, value, **kwargs: print(characteristic, value)
+
+server.write_request_func = write_request
