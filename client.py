1bddc784f23215521c5c58158fb919e15e879f0d
gustav <gustav.hisker@outlook.de>
2024-01-10 00:42:13 +0100

    Logs and so on

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import socket
 
-serverMACAddr = ""
+serverMACAddr = "CC:5E:F8:D7:7A:3C"
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client.connect((serverMACAddr, 4))
 
-
+print("Text an den Server:")
 while True:
-    client.send(bytes(input(),"UTF-8"))
+    client.send(bytes(input(), "UTF-8"))
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import socket
 
-bluetoothMACAddr = ""
+bluetoothMACAddr = "CC:5E:F8:D7:7A:3C"
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.bind((bluetoothMACAddr, 4))
 server.listen(1)
+
+print("Started server")
 
 connection, addr = server.accept()
 
