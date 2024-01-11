dbcbec618ac1484e636be15ac0e3aa3910d5c8b7
gustav <gustav.hisker@outlook.de>
2024-01-11 16:35:15 +0100

    refactored for easier usage

--- send_message.py
+++ send_message.py
@@ -1,12 +1,15 @@
 import socket
 
-with open("Target-Addr") as f:
-    serverMACAddr = f.read().strip()
 
-client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect((serverMACAddr, 4))
+def sendMessage(msg, mac):
+    client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    client.connect((mac, 4))
+    client.send(bytes(msg, "UTF-8"))
+    client.close()
 
-print("Nachricht:")
-client.send(bytes(input(), "UTF-8"))
 
-client.close()
+if __name__ == "__main__":
+    with open("Target-Addr") as f:
+        serverMACAddr = f.read().strip()
+
+    sendMessage(input("Nachricht an " + serverMACAddr + ":\n"), serverMACAddr)
