fa1a2f52724b865581ff4e805301b1fda888de36
gustav <gustav.hisker@outlook.de>
2024-01-12 12:56:22 +0100

    fixed tiny errors

---
+++ client.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='bluedrop',
+    description='Send messages and files to an active bluedrop host.',
+    epilog='Text at the bottom of help')
